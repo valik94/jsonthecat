9bdb122ee1ea4bb8f82742dbd68cc8c7ca4c30da
Val Hustle <stars@MacBook-Air.local>
2021-12-02 19:17:26 -0500

    committing cats as a service with linting, implemneted request to cats API with breedname as the parameter then adding breedName to url and then doing edge case requests error handling. Only thing unsure why else{ callback(null, data[0].description) is throwing me an erorr that the callback is undefined... doesn't make sense

---
+++ breedFetcher.js
@@ -0,0 +1,17 @@
+const request = require('request');
+let breedName = process.argv.slice(2);
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+    }
+    //console.log(typeof body);
+    const data = JSON.parse(body);
+    console.log(typeof data);
+    console.log(typeof data[0].description);
+    if (data.length === 0) {
+      callback('the data in the body is empty', null);
+    } 
+    // else {
+    //   callback(null, data[0].description);
+    // }
+  });
